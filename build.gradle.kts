3ecdbc19047e2da8fadac89d3b2bff1f10bbad60
Ryuusuke-Azuma <azuma.mylabo@gmail.com>
2026-01-03 19:46:57 +0900

    Set repositoriesMode to FAIL_ON_PROJECT_REPOS in settings.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,6 @@
  * Copyright (c) 2026 Ryuusuke Azuma All Rights Reserved.
  */
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 buildscript {
     repositories {
         google()
@@ -12,17 +10,8 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.13.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-        // Compose Multiplatform plugin
         classpath("org.jetbrains.compose:compose-gradle-plugin:1.6.0")
-        // SQLDelight plugin
         classpath("app.cash.sqldelight:gradle-plugin:2.0.1")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
     }
 }
 
