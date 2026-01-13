b2629d3be42161868f466dbb71d9ba4f2c3562e9
Ryuusuke-Azuma <azuma.mylabo@gmail.com>
2026-01-14 00:16:11 +0900

    feat: Share ViewModel among fragments
    Change the ViewModel owner from the Fragment to the Activity to allow data sharing.

--- android/src/main/kotlin/com/mynet/kazekima/wsfighterscard/profile/ProfileFragment.kt
+++ android/src/main/kotlin/com/mynet/kazekima/wsfighterscard/profile/ProfileFragment.kt
@@ -9,7 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
 import com.mynet.kazekima.wsfighterscard.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -18,7 +18,7 @@ class ProfileFragment : Fragment() {
     private val binding get() = _binding!!
     
     // プロフィール専用の ViewModel
-    private val viewModel: ProfileViewModel by viewModels()
+    private val viewModel: ProfileViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
