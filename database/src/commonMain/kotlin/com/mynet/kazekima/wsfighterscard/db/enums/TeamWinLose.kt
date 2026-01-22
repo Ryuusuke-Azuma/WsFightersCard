856211b4d7ac2cb3b09528e8a4df516a786e9c8a
Ryuusuke-Azuma <azuma.mylabo@gmail.com>
2026-01-22 21:24:26 +0900

    Fix: Reorder TeamWinLose enum
    Reorder `TeamWinLose` enum entries to be sorted by wins and then losses, starting from 0-3 and ending with 3-0. The underlying ID values have been updated to reflect this new order.

--- database/src/commonMain/kotlin/com/mynet/kazekima/wsfighterscard/db/enums/TeamWinLose.kt
+++ database/src/commonMain/kotlin/com/mynet/kazekima/wsfighterscard/db/enums/TeamWinLose.kt
@@ -5,10 +5,10 @@
 package com.mynet.kazekima.wsfighterscard.db.enums
 
 enum class TeamWinLose(val id: Long, val label: String, val winLose: WinLose) {
-    WIN_3_0(1, "3-0", WinLose.WIN),
+    LOSE_0_3(0, "0-3", WinLose.LOSE),
+    LOSE_1_2(1, "1-2", WinLose.LOSE),
     WIN_2_1(2, "2-1", WinLose.WIN),
-    LOSE_1_2(3, "1-2", WinLose.LOSE),
-    LOSE_0_3(4, "0-3", WinLose.LOSE);
+    WIN_3_0(3, "3-0", WinLose.WIN);
 
     companion object {
         fun fromId(id: Long): TeamWinLose? = entries.find { it.id == id }
