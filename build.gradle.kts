146add711008c74c169d486b19ee678234bc6bbe
Ryuusuke-Azuma <azuma.mylabo@gmail.com>
2026-03-05 16:59:32 +0900

    Revert "Refactor: Rename "Teams Contribution" to "Teams Detail" in analytics"
    This reverts commit 8cd8e84bf5644b38de3887af66dda802030992e4.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,11 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:9.1.0")
+        classpath("com.android.tools.build:gradle:9.0.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.10")
         classpath("org.jetbrains.kotlin:kotlin-serialization:2.2.10")
         classpath("org.jetbrains.compose:compose-gradle-plugin:1.10.1")
-        classpath("app.cash.sqldelight:gradle-plugin:2.2.1")
+        classpath("app.cash.sqldelight:gradle-plugin:2.0.2")
     }
 }
 
