a596615dc8c05ce4a9f9045b0a9c8e6108be4933
Ryuusuke-Azuma <azuma.mylabo@gmail.com>
2026-07-25 08:01:10 +0900

    Build: Lower minimum SDK to 26
    Update the `MIN_SDK` value from `31` to `26` in `buildSrc/src/main/kotlin/Config.kt`.

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -7,7 +7,7 @@ import org.gradle.api.JavaVersion
 object Config {
     const val COMPILE_SDK = 37
     const val TARGET_SDK = 37
-    const val MIN_SDK = 31
+    const val MIN_SDK = 26
     
     val JAVA_VERSION = JavaVersion.VERSION_21
     const val JVM_TARGET = "21"
